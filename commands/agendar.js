1e60ecdd10e7c7f10e16820291c65aeee53c0de4
Dante Araújo <danteeng@hotmail.com>
2023-04-10 00:03:15 -0300

    Now added it has a regex for tuesday and saturday for checking the next date

--- commands/agendar.js
+++ commands/agendar.js
@@ -9,8 +9,20 @@ const scheduleCommand = new SlashCommandBuilder().setName('agendar').setDescript
 scheduleCommand.addStringOption( option => option.setName('dia').setDescription('Escolhe se prefere terça ou sábado') );
 scheduleCommand.addStringOption( option => option.setName('motivo').setDescription('Diga o motivo da sua dúvida') );
 
-function getNextClass(){
+function getNextClass(next){
   let data = new Date();
+  if (next){
+    const myRegexExpTer = /^(ter(ç|c)?a?)/i;
+    const myRegexExpSab = /^(\bsab(?:ado)?\b)/i;
+    if (myRegexExpTer.test(next)){
+      data = data.next().tuesday();
+    }
+    else if (myRegexExpSab.test(next)){
+      data = data.next().saturday();
+    }
+    return data;
+  }
+
   let daynumber = data.getDay();
   if ( daynumber < 3){
     data = new Date.today().next().tuesday();
@@ -38,26 +50,32 @@ module.exports = {
     const userName = interaction.member.displayName;
     const betterDay = interaction.options.getString('dia') ?? null;
     const reason = interaction.options.getString('motivo') ?? 'Nenhum motivo informado';
+    const sheets = google.sheets({version:'v4', auth});
+
+    const getValuesResponse = await sheets.spreadsheets.values.get({
+      spreadsheetId: config.spreadsheet_id,
+      range:'A:C',
+    })
+    const values = getValuesResponse.data.values || [];
+    let nextDay = ''
+
     if (!betterDay){
-      const nextDay = getNextClass().toString("dd/MM/yy");
-      const sheets = google.sheets({version:'v4', auth});
-
-      const getValuesResponse = await sheets.spreadsheets.values.get({
-        spreadsheetId: config.spreadsheet_id,
-        range:'A:C',
-      })
-      const values = getValuesResponse.data.values || [];
-      const newRow = [[userName,"",nextDay, "" ]];
-
-      await sheets.spreadsheets.values.update({
-        spreadsheetId: config.spreadsheet_id,
-        valueInputOption:'USER_ENTERED',
-        range:`A${values.length + 1}`,
-        resource: {
-          values:newRow,
-        }
-      })
-      await interaction.editReply( `${userName} foi agendado para o próximo tira-dúvidas no dia ${nextDay}` );
+      nextDay = getNextClass().toString("dd/MM/yy");
     }
+    else{
+      nextDay = getNextClass(betterDay).toString("dd/MM/yy");
+    }
+
+    const newRow = [[userName,"",nextDay, "", values[values.length], "", reason ]];
+
+    await sheets.spreadsheets.values.update({
+      spreadsheetId: config.spreadsheet_id,
+      valueInputOption:'USER_ENTERED',
+      range:`A${values.length + 1}`,
+      resource: {
+        values:newRow,
+      }
+    })
+    await interaction.editReply( `${userName} foi agendado para o próximo tira-dúvidas no dia ${nextDay}` );
   }
 }
